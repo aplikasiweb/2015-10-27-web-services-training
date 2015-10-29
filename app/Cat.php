0e22bd3734fbf1aca147094e51c5664d09421e70
Salahuddin Hairai <mr.od3n@gmail.com>
2015-10-29 09:28:34 +0800

    Update Cat model

--- app/Cat.php
+++ app/Cat.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Cat extends Model
 {
-    protected $fillable = [ 'name', 'age' ];
+    //protected $table = 'custom_table'; // default is cats
+    //protected $primaryKey = 'custom_id_column'; // default is id
+    //public $incrementing = true; // set to false if $primaryKey not auto_increment
+    //public $timestamps = true; // set to false to disable eloquent timestamps
+    //const CREATED_AT = 'custom_created_at_column'; // OR null, default is created_at
+    //const UPDATED_AT = 'custom_updated_at_column'; // OR null, default is updated_at
+    protected $fillable = ['name', 'age' ];
 }
